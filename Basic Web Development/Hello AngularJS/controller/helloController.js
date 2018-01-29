9ee1f10e789f6e2d6511cd9272e0148903df4aa7
HuseenMK <hjm3628@rit.edu>
2018-01-29 13:05:14 -0500

    Add files via upload

---
+++ Basic Web Development/Hello AngularJS/controller/helloController.js
@@ -0,0 +1,5 @@
+app.controller('helloController', ['$scope', function($scope) { 
+	$scope.hello = 'Hello! This is Huseen'; 
+  	$scope.img = "me.jpg";
+  	$scope.me = "I love Frontend development and I hate backend !"; 
+}]);
