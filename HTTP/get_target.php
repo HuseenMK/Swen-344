45e2dca6989e4cf82c42641f7c04e8e6f0e5b7d3
HuseenMK <hjm3628@rit.edu>
2018-02-09 21:46:13 -0500

    HTTP

---
+++ HTTP/get_target.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+        
+</head>
+<body>
+    
+  <p>First Name: <?php echo $_GET['FirstName']; ?></p>
+    
+  <p>Last Name: <?php echo $_GET['LastName']; ?></p>
+    
+  <p>Age: <?php echo $_GET['Age']; ?></p>
+    
+  <p>State: <?php echo $_GET['State']; ?></p>
+    
+  <p>Gender: <?php echo $_GET['Gender']; ?></p>
+
+  <h2 style="color:red; text-align:center"  >Notice -> Info is showing in the 
+URL</h2>
+    
+</body>
+</html>
+
---
+++ HTTP/post_target.php
@@ -0,0 +1,21 @@
+<html>
+<head>    
+</head>
+<body>
+    
+  <p>First Name: <?php echo $_POST['FirstName']; ?></p>
+    
+  <p>Last Name: <?php echo $_POST['LastName']; ?></p>
+    
+  <p>Age: <?php echo $_POST['Age']; ?></p>
+    
+  <p>State: <?php echo $_POST['State']; ?></p>
+    
+  <p>Gendeer: <?php echo $_POST['Gender']; ?></p>
+
+  <h2 style="color:red; text-align:center"  >Notic -> Info is not showing in the 
+URL</h2>
+    
+</body>
+</html>
+
